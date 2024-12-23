8e320ebdab1a42bf626bcf7cbf2b53e49c67d452
Haitham Eleuch <haithameleuch@MacBook-Pro-von-Haitham.fritz.box>
2024-12-23 20:57:00 +0100

    added code documentation

--- Projektarbeit/Assets/Scripts/ItemPlacement/ISpawner.cs
+++ Projektarbeit/Assets/Scripts/ItemPlacement/ISpawner.cs
@@ -1,6 +1,12 @@
-using UnityEngine;
-
+/// <summary>
+/// Interface for object spawners in a room. Allows flexibility for spawning different types of objects like items or enemies.
+/// </summary>
 public interface ISpawner
 {
+    /// <summary>
+    /// Spawns objects in a specified room.
+    /// </summary>
+    /// <param name="room">The room in which objects are spawned.</param>
+    /// <param name="isStartRoom">Indicates whether the room is the starting room (no objects should be spawned).</param>
     void SpawnInRoom(RoomBehaviour room, bool isStartRoom);
 }
