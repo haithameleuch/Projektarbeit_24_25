0be8b782d77f68d5de2c29b37f52d63dbb2d9500
Haitham Eleuch <haithameleuch@MBP-von-Haitham.fritz.box>
2024-12-31 06:01:04 +0100

    Updated DungeonGenerator to use RoomData for room creation

--- Projektarbeit/Assets/Scripts/Dungeon/DungeonGenerator.cs
+++ Projektarbeit/Assets/Scripts/Dungeon/DungeonGenerator.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 /// <summary>
@@ -23,6 +24,9 @@ public class DungeonGenerator : MonoBehaviour
         /// [0] Up, [1] Down, [2] Right, [3] Left.
         /// </summary>
         public bool[] Status = new bool[4];
+
+        //TODO: KOMMENTAR
+        public RoomData RoomData;
     }
 
     /// <summary>
@@ -39,10 +43,10 @@ public class DungeonGenerator : MonoBehaviour
     private int startPos;
 
     /// <summary>
-    /// Array of room prefabs used to populate the dungeon.
+    /// Array of room prefabs (Scriptable Objects) used to populate the dungeon.
     /// </summary>
     [SerializeField]
-    private GameObject[] rooms;
+    private RoomData[] roomDataArray;
 
     /// <summary>
     /// Offset distance between rooms in world space.
@@ -75,6 +79,12 @@ public class DungeonGenerator : MonoBehaviour
     /// Counter for the number of cells visited during maze generation.
     /// </summary>
     private int _visitedCells;
+    
+    //TODO: KOMMENTAR
+    private Dictionary<RoomData, int> _usageCount = new();
+    
+    //TODO: KOMMENTAR
+    private Dictionary<Vector2Int, RoomBehaviour> _roomBehaviourMap = new();
 
     /// <summary>
     /// Initializes the dungeon generation process by setting up the grid and generating the maze.
@@ -88,6 +98,9 @@ public class DungeonGenerator : MonoBehaviour
         {
             new ItemSpawner(items, offset)
         };
+
+        //TODO: KOMMENTAR
+        InitializeRoomDataArray(roomDataArray);
         
         MazeGenerator();
     }
@@ -103,12 +116,19 @@ public class DungeonGenerator : MonoBehaviour
         {
             for (var j = 0; j < size.y; j++)
             {
-                Cell currentCell = _board[Mathf.FloorToInt(i + j * size.x)];
+                int cellIndex = Mathf.FloorToInt(i + j * size.x);
+                Cell currentCell = _board[cellIndex];
                 if (currentCell.Visited)
                 {
-                    int randomRoom = Random.Range(0, rooms.Length);
+                    RoomData roomData = currentCell.RoomData;
+                    if (roomData == null)
+                    {
+                        roomData = GetRandomRoomData();
+                        currentCell.RoomData = roomData;
+                    }
+                    
                     var newRoom = Instantiate(
-                            rooms[randomRoom],
+                            roomData.roomPrefab,
                             new Vector3(i * offset.x, 0, -j * offset.y),
                             Quaternion.identity,
                             transform
@@ -117,13 +137,17 @@ public class DungeonGenerator : MonoBehaviour
 
                     newRoom.UpdateRoom(currentCell.Status);
                     newRoom.name += $" {i}-{j}";
+                    
+                    //TODO: KOMMENTAR
+                    _usageCount[roomData]++;
+                    
+                    //TODO: KOMMENTAR (We remember the RoomBehavior for later access)
+                    _roomBehaviourMap[new Vector2Int(i, j)] = newRoom;
 
                     bool isStartRoom = (i == 0 && j == 0);
-                    
-                    // Use all spawners to populate the room
-                    foreach (var spawner in _spawners)
+                    if (roomData.roomType == RoomType.Item)
                     {
-                        spawner.SpawnInRoom(newRoom, isStartRoom);
+                        SpawnItems(newRoom, isStartRoom);
                     }
                 }
             }
@@ -202,7 +226,9 @@ public class DungeonGenerator : MonoBehaviour
             }
         }
 
+        AssignStartRoom();
         GenerateDungeon();
+        EnforceMinCounts();
     }
 
     /// <summary>
@@ -239,5 +265,73 @@ public class DungeonGenerator : MonoBehaviour
         }
 
         return neighbors;
+    }
+    
+    //TODO: KOMMENTAR
+    private void InitializeRoomDataArray(RoomData[] roomDataArr)
+    {
+        foreach (var roomData in roomDataArr)
+        {
+            _usageCount[roomData] = 0;
+        }
+    }
+    
+    //TODO: KOMMENTAR
+    private void SpawnItems(RoomBehaviour room, bool isStartRoom)
+    {
+        // Use all spawners to populate the room
+        foreach (var spawner in _spawners)
+        {
+            spawner.SpawnInRoom(room, isStartRoom);
+        }
+    }
+
+    //TODO: KOMMENTAR
+    private void AssignStartRoom()
+    {
+        RoomData startRoomData = roomDataArray.FirstOrDefault(roomData => roomData.roomType == RoomType.Start);
+        if (startRoomData == null) return;
+
+        int startIndex = 0;
+        _board[startIndex].RoomData = startRoomData;
+    }
+
+    //TODO: KOMMENTAR
+    private RoomData GetRandomRoomData()
+    {
+        var validRoomDataList = new List<RoomData>();
+        foreach (var roomData in roomDataArray)
+        {
+            int usage = _usageCount[roomData];
+
+            // Check macCount
+            if (roomData.maxCount > 0 && usage >= roomData.maxCount)
+            {
+                continue;
+            }
+            validRoomDataList.Add(roomData);
+        }
+
+        // Fallback: use normal room instead
+        if (validRoomDataList.Count == 0)
+        {
+            var normal = roomDataArray.FirstOrDefault(roomData => roomData.roomType == RoomType.Normal);
+            return normal ?? roomDataArray[0];
+        }
+
+        // Otherwise use random room
+        return validRoomDataList[Random.Range(0, validRoomDataList.Count)];
+    }
+
+    //TODO: KOMMENTAR
+    private void EnforceMinCounts()
+    {
+        foreach (RoomData roomData in roomDataArray)
+        {
+            if (_usageCount[roomData] < roomData.minCount)
+            {
+                Debug.Log($"Warning! {roomData.minCount}x \"{roomData.roomName}\" required, but only {_usageCount[roomData]}x set!");
+            }
+        }
     }
 }
