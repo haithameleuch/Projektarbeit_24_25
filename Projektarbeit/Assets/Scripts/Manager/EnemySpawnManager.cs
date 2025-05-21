6ecf75c73a150be6d15fa84afac05083f6719514
armanniaruhi <arman.niaruhi@srudent.uni-siegen>
2025-05-22 01:10:40 +0200

    Delete old material for enemy

--- Projektarbeit/Assets/Scripts/Manager/EnemySpawnManager.cs
+++ Projektarbeit/Assets/Scripts/Manager/EnemySpawnManager.cs
@@ -7,12 +7,6 @@ public class EnemySpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject enemy1 = Instantiate(enemyPrefab, new Vector3(0, 1, 3), Quaternion.identity);
-        enemy1.GetComponent<EnemyBehaviour>().setSpeed(2.0f);
-        GameObject enemy2 = Instantiate(enemyPrefab, new Vector3(-3, 1, 3), Quaternion.identity);
-        enemy2.GetComponent<EnemyBehaviour>().setSpeed(2.5f);
-        GameObject enemy3 = Instantiate(enemyPrefab, new Vector3(-6, 1, 3), Quaternion.identity);
-        enemy3.GetComponent<EnemyBehaviour>().setSpeed(3.0f);
     }
 
     // Update is called once per frame
@@ -21,3 +15,4 @@ public class EnemySpawnManager : MonoBehaviour
         
     }
 }
+^
