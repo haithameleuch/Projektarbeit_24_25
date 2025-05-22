783dd4a2078f0945b714c00048d74f017a4640d8
RexCarnificis <109288871+RexCarnificis@users.noreply.github.com>
2025-05-22 22:14:26 +0200

    added the "e" key functionality

--- Projektarbeit/Assets/Scripts/Manager/UIManager.cs
+++ Projektarbeit/Assets/Scripts/Manager/UIManager.cs
@@ -25,6 +25,14 @@ public class UIManager : MonoBehaviour
     
     private GameObject _player;
 
+    [SerializeField]
+    private GameObject rowPrefab;
+
+    [SerializeField]
+    private GameObject slotPrefab;
+
+    [SerializeField]
+    private GameObject emptyPrefab;
 
     /// <summary>
     /// Ensures there is only one instance of the UIManager in the scene.
@@ -114,13 +122,89 @@ public class UIManager : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.E))
         {
+            GameObject inv = gameObject.transform.Find("Inventory").gameObject;
+
             if (!isInvVisible)
             {
+                HidePanel();
+
+                player.SetActive(false);
+
+                renderInv(inv);
+                inv.SetActive(true);
+
+                //Make the cursor moveable within the game window
+                UnityEngine.Cursor.lockState = CursorLockMode.Confined;
+                UnityEngine.Cursor.visible = true;
+
+                //Pause the game
+                Time.timeScale = 0;
+
                 isInvVisible = true;
             }
             else
             {
+
+                player.SetActive(true);
+
+                inv.SetActive(false);
+
+                deRenderInv(inv);
+
+                //Make the cursor unmoveable within the game window
+                UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+                UnityEngine.Cursor.visible = false;
+
+                //Pause the game
+                Time.timeScale = 1;
+
                 isInvVisible = false;
+            }
+        }
+    }
+
+    private void deRenderInv(GameObject inv)
+    {
+        GameObject items = inv.transform.Find("Items").gameObject;
+        foreach (Transform child in items.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        GameObject equipslots = inv.transform.Find("Equipment").gameObject.transform.Find("EquipmentSlots").gameObject;
+
+        foreach (Transform child in equipslots.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    private void renderInv(GameObject inv)
+    {
+        GameObject items = inv.transform.Find("Items").gameObject;
+        for (int i = 0; i < 4; i++)
+        {
+            GameObject row = Instantiate(rowPrefab,items.transform);
+            for (int j=0; j<5;j++)
+            {
+                GameObject slot = Instantiate(emptyPrefab, row.transform);
+            }
+        }
+
+        GameObject equip = inv.transform.Find("Equipment").gameObject;
+
+        GameObject equipslots = equip.transform.Find("EquipmentSlots").gameObject;
+
+        TMP_Text stats = equip.transform.Find("Stats").gameObject.transform.Find("StatDetails").GetComponent<TMP_Text>();
+
+        stats.SetText("Health:10\nDamage:10\nSpeed:10");
+
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject row = Instantiate(rowPrefab, equipslots.transform);
+            for (int j = 0; j < 2; j++)
+            {
+                GameObject slot = Instantiate(emptyPrefab, row.transform);
             }
         }
     }
