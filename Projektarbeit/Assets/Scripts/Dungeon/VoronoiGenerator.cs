1cd0762b9d13b69f5e81a0cce5db146d8f3ebcbb
Haitham El Euch <ehaitham592@yahoo.de>
2025-05-29 15:27:22 +0200

    added destroyable wall prefab

--- Projektarbeit/Assets/Scripts/Dungeon/VoronoiGenerator.cs
+++ Projektarbeit/Assets/Scripts/Dungeon/VoronoiGenerator.cs
@@ -19,6 +19,8 @@ public class VoronoiGenerator : MonoBehaviour
     private GameObject floor;
     [SerializeField]
     private GameObject door;
+    [SerializeField]
+    private GameObject destroyableWall;
     
     [Header("Dungeon Settings")]
     [SerializeField]
