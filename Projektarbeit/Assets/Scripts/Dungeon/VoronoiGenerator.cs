9f8a6b5468b41f688434b2ea876a089f26afd99d
Haitham El Euch <ehaitham592@yahoo.de>
2025-07-04 02:25:15 +0200

    fixed destroyable wall spawn

--- Projektarbeit/Assets/Scripts/Dungeon/VoronoiGenerator.cs
+++ Projektarbeit/Assets/Scripts/Dungeon/VoronoiGenerator.cs
@@ -277,7 +277,7 @@ public class VoronoiGenerator : MonoBehaviour
             }
 
             bool forceDoor = forceBossDoor || forcePathDoor;
-            bool forbidBreakable = roomsWithEdge.Any(r => r.type == RoomType.Boss || r.type == RoomType.Enemy); // TODO: ENEMY ROOMS NOT DESTROYABLE WALLS
+            bool forbidBreakable = roomsWithEdge.Any(r => r.type == RoomType.Boss);
             bool placeDoor = forceDoor || (_rng.NextDouble() < DoorProbability);
 
             bool isMiddle   = i == midIndex;
@@ -303,7 +303,7 @@ public class VoronoiGenerator : MonoBehaviour
 
                 _dungeonGraph.idDoorDict[edgeId] = doorObj;
             }
-            else if (candidate && !forbidBreakable) // TODO: ENEMY ROOMS NOT DESTROYABLE WALLS (!forbidBreakable)
+            else if (candidate && !forbidBreakable)
             {
                 // Place a breakable wall instead of a door
                 Vector3 mid = (segStart + segEnd) * 0.5f;
