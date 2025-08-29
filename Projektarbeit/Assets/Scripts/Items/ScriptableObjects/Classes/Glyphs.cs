553b7ea788dcdb427b8ce60ec0a694d9da0ea657
RexCarnificis <109288871+RexCarnificis@users.noreply.github.com>
2025-08-29 11:25:27 +0200

    added air glyph as item

---
+++ Projektarbeit/Assets/Scripts/Items/ScriptableObjects/Classes/Glyphs.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Glyphs", menuName = "Scriptable Objects/Glyphs")]
+public class Glyphs : Item
+{
+    public override void use(Inventory inv)
+    {
+    }
+}
