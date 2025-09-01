b4afbf92f4503b5442118916a8c37b7b068711c9
Haitham El Euch <ehaitham592@yahoo.de>
2025-09-02 01:37:51 +0200

    revert changes show and hide drawing canvas infos

--- Projektarbeit/Assets/Scripts/Manager/CameraManager.cs
+++ Projektarbeit/Assets/Scripts/Manager/CameraManager.cs
@@ -88,7 +88,6 @@ public class CameraManager : MonoBehaviour
         SetPlayerVisible(true);
         _firstPersonPlayerController.enabled = true;
         CanvasDraw.ToDraw = false;
-        ActiveCanvasDraw = null;
     }
 
     /// <summary>
