f621e1abf3969003c723a8289f9a1265f02b6ed7
root <root@DESKTOP-E3ODJO2.localdomain>
2025-09-10 20:28:28 +0200

    add comments for describing functions and classes

--- Projektarbeit/Assets/Scripts/ItemPlacement/BossSpawnerVoronoi.cs
+++ Projektarbeit/Assets/Scripts/ItemPlacement/BossSpawnerVoronoi.cs
@@ -5,19 +5,59 @@ using Saving;
 
 namespace ItemPlacement
 {
+    /// <summary>
+    /// class to manage boss & obstacle spawning, setting the boss active and inactive
+    /// and also spawning the exit prefab to leave current level and proceed
+    /// </summary>
     public class BossSpawnerVoronoi
     {
+        /// <summary>
+        /// Stores all bosses per room by room id as the key which are spawned
+        /// </summary>
         private readonly Dictionary<int, List<GameObject>> _bossInstancesPerRoom = new();
+        /// <summary>
+        /// Stores the amount of enemies which are alive per boss room by room id as the key
+        /// </summary>
         private readonly Dictionary<int, int> _alivePerRoom = new();
+        /// <summary>
+        /// stores the id of the current boss room 
+        /// </summary>
         private readonly int _bossRoomId = -1;
 
+        /// <summary>
+        /// Parent transform under which the enemies are spawned
+        /// </summary>
         private readonly Transform _parent;
+        
+        /// <summary>
+        /// Prefab which will be spawned after the boss has been defeated
+        /// </summary>
         private readonly GameObject _levelExitPrefab;
+        
+        /// <summary>
+        /// Room metadata object reference of the current boss room
+        /// </summary>
         private Room _bossRoomRef;
+        
+        /// <summary>
+        /// private bool member if the exit has been spawned already 
+        /// </summary>
         private bool _exitSpawned;
         
+        /// <summary>
+        /// static instance of BossSpawnerVoronoi
+        /// </summary>
         private static BossSpawnerVoronoi _instance;
 
+        /// <summary>
+        /// constructor of Boss Spawner Voronoi which will set the members, creates the static instance,
+        /// spawnes the boss prefab & obstacles in the corresponding bossRoom
+        /// </summary>
+        /// <param name="bossRoom">room data of boss room into which the boss will be spawned</param>
+        /// <param name="bossPrefabs">List of boss prefabs which will be used to spawn </param>
+        /// <param name="obstaclePrefabs">List of prefabs that will be spawned as obstacles</param>
+        /// <param name="parent">Parent transform under which the enemies are spawned</param>
+        /// <param name="levelExitPrefab">Prefab which will be spawned after beating the boss</param>
         public BossSpawnerVoronoi(Room bossRoom, List<GameObject> bossPrefabs, List<GameObject> obstaclePrefabs, Transform parent, GameObject levelExitPrefab = null)
         {
             _instance = this;
@@ -105,6 +145,10 @@ namespace ItemPlacement
             _alivePerRoom[bossRoom.id] = spawned.Count;
         }
         
+        /// <summary>
+        /// sets the boss' gameobjects in the corresponding room active 
+        /// </summary>
+        /// <param name="room">id to room where gameobjects/enemies should be activated</param>
         public void ActivateBossInRoom(Room room)
         {
             if (room == null || room.id != _bossRoomId) return;
@@ -116,6 +160,12 @@ namespace ItemPlacement
             }
         }
 
+        /// <summary>
+        /// function that tracks if all enemies in the corresponding boss room have been defeated and the exit prefab has been spawned
+        /// if yes it returns, else it will spawn the exit and sets _exitSpawned to true
+        /// decreases living amount of the room
+        /// </summary>
+        /// <param name="roomId">room id of the boss room where the boss has been defeated</param>
         private void OnBossEnemyDied(int roomId)
         {
             if (!_alivePerRoom.ContainsKey(roomId)) return;
@@ -134,6 +184,9 @@ namespace ItemPlacement
             _exitSpawned = true;
         }
         
+        /// <summary>
+        /// instantiates level exit prefab to the correspoinding boss room and uses the parent of the room
+        /// </summary>
         private void SpawnLevelExit()
         {
             if (_bossRoomRef == null || _levelExitPrefab == null) return;
@@ -154,16 +207,28 @@ namespace ItemPlacement
                 exit.AddComponent<LevelExitInteraction>();
         }
         
+        /// <summary>
+        /// increases amount of living enemies in the corresponding room because the boss can spawn smaller enemies to his help
+        /// </summary>
+        /// <param name="roomId">id of boss room where living amount should be increased</param>
         public static void RegisterBossMinionSpawn(int roomId)
         {
             _instance?.IncrementAlive(roomId);
         }
 
+        /// <summary>
+        /// reduces amount of living enemies in the corresponding room because the boss can spawn smaller enemies to his help
+        /// </summary>
+        /// <param name="roomId">id of boss room where living amount should be decreased</param>
         public static void RegisterBossMinionDeath(int roomId)
         {
             _instance?.OnBossEnemyDied(roomId);
         }
         
+        /// <summary>
+        /// function that increases the tracker of the living enemies per room
+        /// </summary>
+        /// <param name="roomId">id of boss room where living amount should be increased</param>
         private void IncrementAlive(int roomId)
         {
             if (!_alivePerRoom.ContainsKey(roomId))
--- Projektarbeit/Assets/Scripts/Spawning/Distributor.cs
+++ Projektarbeit/Assets/Scripts/Spawning/Distributor.cs
@@ -2,84 +2,105 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-/// <summary>
-/// Handles the distribution of elements (items, enemies or other distributables)
-/// 
-/// </summary>
-public class Distributor<T> where T : ItemInstance, new()
+namespace Spawning
 {
     /// <summary>
-    /// elements, which should be distributed
+    /// Handles the distribution of elements (items, enemies or other distributables)
+    /// and returns & stores the elements which should be spawned
     /// </summary>
-    private List<T> _randomElements;
-    private List<T> _mustElements;
-    /// <summary>
-    /// probabilities of elements normalized if the cumulated probability of _randomElements != 100
-    /// is calculated when an object will be instantiated
-    /// </summary>
-    private List<float> _normalizedProbabilities;
+    public class Distributor<T> where T : ItemInstance, new()
+    {
+        /// <summary>
+        /// elements, which should be distributed
+        /// </summary>
+        private List<T> _randomElements;
+        /// <summary>
+        /// elements, which MUST be spawned. Will be reduced to 0 after all elements have been spawned
+        /// </summary>
+        private List<T> _mustElements;
+        /// <summary>
+        /// probabilities of elements normalized if the cumulated probability of _randomElements != 100
+        /// is calculated when an object will be instantiated
+        /// </summary>
+        private List<float> _normalizedProbabilities;
     
-    public int MustCount => _mustElements?.Count ?? 0;
+        /// <summary>
+        /// number of _mustElements which are necessary to be spawned
+        /// </summary>
+        public int MustCount => _mustElements?.Count ?? 0;
 
-    /// <summary>
-    /// Initializes a new instance of the Distributor class, sets the _randomElements
-    /// and calculates the normalized probabilities for each element in the same order as the randomElements appear in _randomElements
-    /// </summary>
-    /// <param name="randomElements">List of randomElements which should be distributed.</param>
-    public Distributor(List<T> randomElements){
-        if (!randomElements.Any())
-        {
-            return;
-        }
-        var cumulativeProbability = 0.0f;
-        
-        foreach (T element in randomElements)
-        {
-            cumulativeProbability += element.itemData.rarity;
-        }
-        
-        var elementProbability = 0.0f;
-        foreach (var element in randomElements)
-        {
-            var normalizedProbability = element.itemData.rarity / cumulativeProbability;
-            if (elementProbability == 0.0f)
+        /// <summary>
+        /// Initializes a new instance of the Distributor class, sets the _randomElements
+        /// and calculates the normalized probabilities for each element in the same order as the randomElements appear in _randomElements
+        /// </summary>
+        /// <param name="randomElements">List of randomElements which should be distributed.</param>
+        public Distributor(List<T> randomElements){
+            if (!randomElements.Any())
             {
-                elementProbability += normalizedProbability;
-                _normalizedProbabilities = new List<float>{elementProbability};
-                continue;
+                return;
             }
-            elementProbability += normalizedProbability;
-            _normalizedProbabilities.Add(elementProbability);
+            var cumulativeProbability = 0.0f;
+        
+            foreach (T element in randomElements)
+            {
+                cumulativeProbability += element.itemData.rarity;
+            }
+        
+            var elementProbability = 0.0f;
+            foreach (var element in randomElements)
+            {
+                var normalizedProbability = element.itemData.rarity / cumulativeProbability;
+                if (elementProbability == 0.0f)
+                {
+                    elementProbability += normalizedProbability;
+                    _normalizedProbabilities = new List<float>{elementProbability};
+                    continue;
+                }
+                elementProbability += normalizedProbability;
+                _normalizedProbabilities.Add(elementProbability);
+            }
+            _randomElements = randomElements;
         }
-        _randomElements = randomElements;
-    }
+    
+        /// <summary>
+        /// Initializes a new instance of the Distributor class, sets the _randomElements & _mustElements
+        /// and calculates the normalized probabilities for each element in the same order as the randomElements appear in _randomElements
+        /// </summary>
+        /// <param name="randomElements">List of randomElements which should be distributed.</param>
+        /// <param name="mustElements">list of elements that are necessary to be spawned</param>
+        public Distributor(List<T> randomElements, List<T> mustElements) : this(randomElements)
+        {
+            _mustElements = mustElements;
+        }
 
-    public Distributor(List<T> randomElements, List<T> mustElements) : this(randomElements)
-    {
-        _mustElements = mustElements;
-    }
-
-    /// <summary>
-    /// function for retrieving a random element out of the _randomElements
-    /// first a random value is generated and then compared where this value is smaller or at least equal to a value
-    /// and the index of this element where this suffices for the first time is the index of the element which is wanted
-    /// bc the order of the normalized probabilities is the same as in _randomElements
-    /// </summary>
-    /// <returns>element out of _randomElements</returns>
-    public T GetRandomElement()
-    {
-        if (_randomElements.Count == 0) return new T();
-        var randomValue = Random.Range(0.0f, 1.0f);
-        var elementIndex = _normalizedProbabilities.FindIndex(x => x >= randomValue);
-        return _randomElements[elementIndex];
-    }
-
-    public T GetRandomElementIncludingMust()
-    {
-        if (_mustElements.Count <= 0) return GetRandomElement();
-        var element = _mustElements.First();
-        _mustElements.Remove(element);
-        return element;
+        /// <summary>
+        /// function for retrieving a random element out of the _randomElements
+        /// first a random value is generated and then compared where this value is smaller or at least equal to a value
+        /// and the index of this element where this suffices for the first time is the index of the element which is wanted
+        /// bc the order of the normalized probabilities is the same as in _randomElements
+        /// </summary>
+        /// <returns>element out of _randomElements</returns>
+        public T GetRandomElement()
+        {
+            if (_randomElements.Count == 0) return new T();
+            var randomValue = Random.Range(0.0f, 1.0f);
+            var elementIndex = _normalizedProbabilities.FindIndex(x => x >= randomValue);
+            return _randomElements[elementIndex];
+        }
+    
+        /// <summary>
+        /// function for retrieving an element out the elements that are available
+        /// first it will be checked if there is an element in the _mustElements list remaining and if all are used then
+        /// GetRandomElement is used to get a random element
+        /// </summary>
+        /// <returns>element out of _randomElements or _mustElements</returns>
+        public T GetRandomElementIncludingMust()
+        {
+            if (_mustElements.Count <= 0) return GetRandomElement();
+            var element = _mustElements.First();
+            _mustElements.Remove(element);
+            return element;
+        }
     }
 }
 
